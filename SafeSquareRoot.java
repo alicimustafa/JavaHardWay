5b9b3ccb64b03fbf8d2948b7c0fdb439ffd3306a
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-07-26 09:41:55 -0600

    add exersize 28

---
+++ SafeSquareRoot.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SafeSquareRoot {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    double x, y;
+    String ready;
+
+    System.out.print("Are you ready: ");
+    ready = keyboard.next();
+
+    while (!ready.equals("YES")){
+      ready = keyboard.next();
+    }
+
+    System.out.print("Give me a number, and I'll fing it's square root. ");
+    System.out.print("(No negative, please.)");
+    x = keyboard.nextDouble();
+
+    while (x < 0){
+      System.out.println("I won't take the square root of negative.");
+      System.out.print("\nNew number");
+      x = keyboard.nextDouble();
+    }
+
+    y = Math.sqrt(x);
+
+    System.out.println("The square root of " + x  + " is " + y);
+  }
+}
